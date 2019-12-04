f8a4b79c0c52f755d3c80199dd5f51341b0ac377
RYAN ALEX FRANKLIN <57727779+RyCodes@users.noreply.github.com>
2019-12-04 12:39:46 -0600

    Add files via upload

--- LateChangeProject2-master/team03_project2/simulator.py
+++ LateChangeProject2-master/team03_project2/simulator.py
@@ -111,78 +111,85 @@ class Simulator():
             jumpAddr = armState.PC
             i = armState.getIndexOfMemAddress(armState.PC)
 
-            # NOP
+            # NOP: No Operation.
             if (self.opcode[i] == 0):
                 armState.printState()
                 armState.incrementPC()
                 armState.cycle += 1
                 continue
 
-            # B
+            # B: Extends and shifts (multipies by 4) the 26 bit argument (words) in offset and adds the
+            # value to the PC.
             elif (160 <= self.opcode[i] <= 191):
                 jumpAddr = jumpAddr + ((self.arg1[i] * 4) - 4)
 
-            # AND type
+            # AND: Rn register & Rm  register result into Rd register.
             elif self.opcode[i] == 1104:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] & armState.R[self.arg1[i]]
 
-            # ADD
+            # ADD: Rn register + Rm  register result into Rd register.
             elif self.opcode[i] == 1112:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] + armState.R[self.arg1[i]]
 
-            # ADDI
+            # ADDI Adds extended immediate value to the value in Rn register
+            # and puts value in Rd  register. Immediate is positive.
             if 1160 <= self.opcode[i] <= 1161:
                 armState.R[self.arg3[i]] = self.arg2[i] + armState.R[self.arg1[i]]
 
-            # ORR
+            # ORR: Rn register or Rm  register result into Rd register.
             elif self.opcode[i] == 1360:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] | armState.R[self.arg1[i]]
 
-            # CBZ signed
+            # CBZ: Does comparison against zero of condition register and if condition met adds extended
+            # and shifted offset to the PC. Offset can be positive or negative.  Offset in words.
             elif 1440 <= self.opcode[i] <= 1447:
                 if self.arg2[i] == 0:
                     jumpAddr = jumpAddr + ((self.arg1[i] * 4) - 4)
 
-            # CBNZ signed
+            # CBNZ: Does comparison against zero of condition register and if condition met adds extended
+            # and shifted offset to the PC. Offset can be positive or negative.  Offset in words.
             elif 1448 <= self.opcode[i] <= 1455:
                 if self.arg2[i] != 0:
                     jumpAddr = jumpAddr + ((self.arg1[i] * 4) - 4)
 
-            # SUB
+            # SUB:  Rn register - Rm  register result into Rd register.
             elif self.opcode[i] == 1624:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] - armState.R[self.arg1[i]]
 
-            # SUBI
+            # SUBI: Subtracts extended immediate value from the value in Rn register and puts value in
+            # Rd register. Immediate is positive.
             elif 1672 <= self.opcode[i] <= 1673:
                 armState.R[self.arg3[i]] = self.arg2[i] - armState.R[self.arg1[i]]
 
 
-            # MOVZ
+            # MOVZ: 16 bit pattern in field register shifted left by either 0,16,32,48 positions determined
+            # by 2 bit shift code value and written into zeroed Rd register.
             elif 1684 <= self.opcode[i] <= 1687:
                 armState.R[self.arg2[i]] = 0
                 armState.R[self.arg2[i]] = armState.R[self.arg3[i]] << armState.R[self.arg1[i]]
 
-            # LSR
+            # LSR: This is a pattern shift. Whatever is in register is shifted right with zero fill.
             elif self.opcode[i] == 1690:
-                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] / 2 ** armState.R[self.arg1[i]]
-
-            # LSL
+                # armState.R[self.arg3[i]] = armState.R[self.arg2[i]] / 2 ** armState.R[self.arg1[i]]
+                armState.R[self.arg3[i]] = armState.R[self.arg2[i]] % (1 << 32) >> self.arg1[i]
+            # LSL: Shift left arg2 positions adding zeros on the right.
             elif self.opcode[i] == 1691:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] << armState.R[self.arg1[i]]
 
-            # ASR
+            # ASR: This is  ">>" will divide by 2.
             elif self.opcode[i] == 1692:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] >> armState.R[self.arg1[i]]
 
-            # EOR
+            # EOR: Rn register ^ Rm  register result into Rd register.
             elif self.opcode[i] == 1872:
                 armState.R[self.arg3[i]] = armState.R[self.arg2[i]] ^ armState.R[self.arg1[i]]
 
-            # MOVK
+            # MOVK: 16 bit pattern in field  register shifted left by either 0,16,32,48 positions determined by
+            # 2 bit shift code value and written into  a register leaving all other bits intact.
             elif 1940 <= self.opcode[i] <= 1943:
                 armState.R[self.arg2[i]] = armState.R[self.arg3[i]] << armState.R[self.arg1[i]]
 
-            # STUR get data from register RT
+            # STUR: get data from register RT and store that data in memory
             elif self.opcode[i] == 1984:
                 targetAddress = (armState.R[self.arg1[i]] + (self.arg2[i] * 4))
                 while armState.getIndexOfMemAddress(targetAddress) == -1:
@@ -190,7 +197,7 @@ class Simulator():
                     self.address.append(max(self.address) + 4)
                 self.dataVal[armState.getIndexOfMemAddress(targetAddress)-self.numInstructions+nonUpdatedDataValLength] = armState.R[self.arg3[i]]
 
-            # LDUR get data from memory and store in Register RT
+            # LDUR: get data from memory and store in Register RT
             elif self.opcode[i] == 1986:
                 targetAddress = (armState.R[self.arg1[i]] + (self.arg2[i] * 4))
                 while armState.getIndexOfMemAddress(targetAddress) == -1:
@@ -198,13 +205,14 @@ class Simulator():
                     self.address.append(max(self.address) + 4)
                 armState.R[self.arg3[i]] = self.dataVal[armState.getIndexOfMemAddress(targetAddress)-self.numInstructions+nonUpdatedDataValLength]
 
-            # BREAK
+            # BREAK:
             elif self.opcode[i] == 2038:
                 foundBreak = True
 
             else:
                 print("IN SIM -- UNKNOWN INSTRUCTION---------!!!!!")
 
+            # For feng shui of output
             while len(self.address[self.numInstructions:]) % 8 != 0:
                 for j in range(len(self.address[self.numInstructions:]) % 8):
                     self.dataVal.append(0)
